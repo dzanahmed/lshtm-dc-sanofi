a44e4da995508dbd1d26e2f961897362de305dba
dzanahmed <55654315+dzanahmed@users.noreply.github.com>
2023-01-18 09:05:40 +0000

    Script edit for DE cleaning

--- scripts/cleaning/DE/Script_All.R
+++ scripts/cleaning/DE/Script_All.R
@@ -17,14 +17,16 @@ for (file in files) {
      pathogen <- substr(file, start = nchar(dir) + 10, stop = nchar(dir) + 12)
      
      df[is.na(df)] <- 0 # Set all NAs to 0s
-     df$year <- as.numeric(year)
-     df$pathogen <- pathogen
+     df$Year <- as.numeric(year)
+     df$Pathogen <- pathogen
      
      all_data <- rbind(all_data, df)
 }
 
-all_data[2:20] <- as.numeric(unlist(all_data[2:20]))
-all_data$pathogen[all_data$pathogen=="COV"] <- "COVID-19"
+all_data[2:20] <- as.numeric(unlist(all_data[2:20])) # convert to numeric
+all_data$Pathogen[all_data$pathogen=="COV"] <- "COVID-19" # rename
 
+# Reorder variables and remove Unknown as there are no values present other than 0
+all_data <- all_data |> dplyr::select(Week, Year, Pathogen, Total, starts_with('A'))
 
 write.csv(all_data, file = 'data/processed_data/DE/DE_Pathogens_Years_Weekly_Age_breakdown.csv')
