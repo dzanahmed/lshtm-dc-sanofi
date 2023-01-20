e74df0714eb91ecbf00dd309a9dffb2f8ca012d4
jjcato9 <7caterson@gmail.com>
2023-01-20 14:38:53 +0000

    UK 2017 graph with seasonal x-axis

--- scripts/absolute_hospitalisation_plots.R
+++ scripts/absolute_hospitalisation_plots.R
@@ -6,19 +6,40 @@ require(ggplot2)
 require(tidyverse) 
 
 #read merged data file
-data <- read.csv('data/merged_data/first_output_2022_01_19.csv')
+data <- read.csv('data/merged_data/merged_data.csv')
 head(data) 
 
-#read epi file
-epi_weeks <- read.csv('epiweeks_withdate.csv')
+#change start_date to data
+data$start_date <- as.Date(data$start_date)
 
+class(data$start_date)
 
-#join merged data and epi weeks
+#set up data sets by season
 
-data <- merge(x=data,y=epi_weeks,by.x = c('week','year'), by.y = c('MMWR_WEEK','MMWR_YEAR'),all.x=TRUE)
-data$MMWR_WEEKSTARTDATE <- as.Date(data$MMWR_WEEKSTARTDATE) #convert to datetime
+# 2017 - start wk - 2017-10-01 --> 2018-05-13
+data_17 <- data %>% filter(start_date >= '2017-10-01')
+data_17 <- data_17 %>% filter(start_date <= '2018-05-13')
 
-class(data$MMWR_WEEKSTARTDATE)
+#test plot seasonal data
+
+uk_data_17 <- data_17 %>% filter(country == 'UK')
+uk_data_17 <- uk_data_17 %>% filter(age_group == 'ALL')
+
+# create Epi Week Labels
+
+date_breaks <- uk_data_17$start_date
+epi <- uk_data_17$week
+
+ggplot() + geom_line(data=uk_data_17,aes(x=start_date,y=hsp_abs_flu),color='red') +
+     geom_line(data=uk_data_17,aes(x=start_date,y=hsp_abs_rsv),color='orange') +
+     geom_col(data=uk_data_17,aes(x=start_date,y=hsp_abs),alpha=0.2) +
+     xlab('Epi Week') +
+     ylab('No. Hospitalisations') +
+     ggtitle('Total Hospitalisations and Hospitalisations by Virus for UK in 2017/18 Season') +
+     theme_bw() +
+     scale_x_date(breaks = date_breaks,labels=epi) +
+     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
 
 # test plot with week data
 
@@ -37,3 +58,4 @@ ggplot() + geom_line(data=uk_data_22,aes(x=week,y=hsp_abs_covid),color='dark blu
      theme_bw()
 
 # test plot with seasons as x axis
+
