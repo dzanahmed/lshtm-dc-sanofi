80dc6fca01a78286a1215c33bc1fc36dcfb3706e
dzanahmed <55654315+dzanahmed@users.noreply.github.com>
2023-01-26 10:44:05 +0000

    Rendering Fig 01

--- scripts/analysis/Fig01_hospitalization_rates_hemisphere.R
+++ scripts/analysis/Fig01_hospitalization_rates_hemisphere.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(lubridate)
 
-data <- read.csv(file='data/merged_data/merged_data.csv')
+data <- read.csv(file='data/merged_data/merged_data2.csv')
 
 # Overall hospitalization by virus (Influenza, RSV, COVID-19) for the NH and SH 
 # (complete time series for full time period: 2016 - 2023)
@@ -15,6 +15,12 @@ epi <- data$week
 
 breaks_mo <- seq(min(data$start_date), max(data$start_date), by="4 months")
 
+x <- data |> filter(age_group=="ALL") |> 
+        mutate(hsp_x=hsp_rate_flu+hsp_rate_rsv+ifelse(is.na(hsp_rate_covid19)==TRUE,0,hsp_rate_covid19))
+
+
+
+?sum
 
 # this is messy 
 
@@ -50,13 +56,14 @@ breaks_mo <- seq(min(data$start_date), max(data$start_date), by="4 months")
 #                                  TRUE ~ hsp_rate_rsv))
 
 
-Figure_1_NH <- data |> filter(hemisphere=='NH', age_group=="ALL", data_source!="FluNet - Sentinel") |> 
+Figure_1_NH <- x |> filter(hemisphere=='NH', age_group=="ALL", data_source!="FluNet - Sentinel") |> 
      filter(country!="UK" | year!=2020 | week<15) |> 
      ggplot()+
      geom_line(mapping = aes(start_date, hsp_rate_flu, color="Influenza")) +
      geom_line(mapping = aes(start_date, hsp_rate_rsv, color="RSV")) +
      geom_line(mapping=aes(start_date,hsp_rate_covid19,color='SARS_CoV_2')) +
-        scale_color_manual("", 
+        geom_col(mapping=aes(start_date, hsp_x), alpha=0.2)+
+     scale_color_manual("", 
                            breaks = c('Influenza','RSV','SARS_CoV_2'),
                            values = c("Influenza"="red", "RSV"="orange",'SARS_CoV_2'='blue'))+
      scale_x_date(date_labels="%b", date_breaks="month", expand=c(0.01,0)) +
@@ -74,15 +81,17 @@ Figure_1_NH <- data |> filter(hemisphere=='NH', age_group=="ALL", data_source!="
      plot.subtitle = element_text(hjust=0.5))+
      theme(legend.position = 'bottom')+
      labs(title="Influenza and RSV hospitalizations in the Northern hemisphere, seasons 2016-2019 and 2022-23",
-          subtitle="Rates per 100,000", x="Time", y="Hospitalizations (n/100,000)", caption="Preliminary output Dzan")
+          subtitle="Rates per 100,000", x="Time", y="Hospitalizations (n/100,000)", caption="")
 
 Figure_1_NH
 
-Figure_1_SH <- data |> filter(hemisphere=='SH', age_group=="ALL") |> 
+Figure_1_SH <- x |> filter(hemisphere=='SH', age_group=="ALL") |> 
      filter(data_source!="AUS DOH") |> 
      ggplot()+
-        geom_line(mapping = aes(start_date, hsp_rate_flu, color="Influenza")) +
+        geom_line(mapping = aes(start_date, hsp_rate_flu, color="Influenza")) +     
         geom_line(mapping = aes(start_date, hsp_rate_rsv, color="RSV")) +
+        geom_line(mapping=aes(start_date,hsp_rate_covid19,color='SARS_CoV_2')) +
+        geom_col(mapping=aes(start_date, hsp_x), alpha=0.2)+
         scale_color_manual("", 
                            breaks = c('Influenza','RSV','SARS_CoV_2'),
                            values = c("Influenza"="red", "RSV"="orange",'SARS_CoV_2'='blue'))+
@@ -101,19 +110,21 @@ Figure_1_SH <- data |> filter(hemisphere=='SH', age_group=="ALL") |>
      plot.subtitle = element_text(hjust=0.5))+
      theme(legend.position = 'bottom')+
      labs(title="Influenza and RSV hospitalizations in the Southern hemisphere, seasons 2016-2019 and 2022-23",
-          subtitle="Rates per 100,000", x="Time", y="Hospitalizations (n/100,000)", caption="Preliminary output Dzan")
+          subtitle="Rates per 100,000", x="Time", y="Hospitalizations (n/100,000)", caption="")
 
 Figure_1_SH
 
-Figure_1_Both <- data |> filter(age_group=="ALL") |> 
+Figure_1_Both <- x |> filter(age_group=="ALL") |> 
      filter(data_source!="AUS DOH", data_source!="FluNet - Sentinel") |> 
      filter(country!="UK" | year!=2020 | week<15) |> 
      ggplot()+
         geom_line(mapping = aes(start_date, hsp_rate_flu, color="Influenza")) +
         geom_line(mapping = aes(start_date, hsp_rate_rsv, color="RSV")) +
-        scale_color_manual("",
-                           breaks = c('Influenza','RSV'),
-                           values = c("Influenza"="red", "RSV"="orange"))+
+        geom_line(mapping=aes(start_date,hsp_rate_covid19,color='SARS_CoV_2')) +
+        geom_col(mapping=aes(start_date, hsp_x), alpha=0.2)+
+        scale_color_manual("", 
+                           breaks = c('Influenza','RSV','SARS_CoV_2'),
+                           values = c("Influenza"="red", "RSV"="orange",'SARS_CoV_2'='blue'))+
      scale_x_date(date_labels="%b", date_breaks="month", expand=c(0.01,0)) +
      facet_grid(country ~ year(start_date), space="free_x", scales="free_x", switch="x") +
      theme_bw() +
@@ -130,13 +141,13 @@ Figure_1_Both <- data |> filter(age_group=="ALL") |>
      scale_y_sqrt()+
      theme(legend.position = 'bottom')+
      labs(title="Influenza and RSV hospitalizations in NH and SH, seasons 2016-2019 and 2022-23",
-          subtitle="Rates per 100,000", x="Time", y="Hospitalizations (n/100,000)", caption="Preliminary output Dzan")
+          subtitle="Rates per 100,000", x="Time", y="Hospitalizations (n/100,000)", caption="")
 
 Figure_1_Both
 
 
 ggsave(
-        'output/Fig 01 - Hospitalization rates per 100k/Figure_1_NH.png',
+        'output/Fig 01 - Hospitalization rates per 100k/Figure_1_NH_20220126.png',
         Figure_1_NH,
         width=16,
         height=9
@@ -144,14 +155,14 @@ ggsave(
 
 
 ggsave(
-        'output/Fig 01 - Hospitalization rates per 100k/Figure_1_SH.png',
+        'output/Fig 01 - Hospitalization rates per 100k/Figure_1_SH_20220126.png',
         Figure_1_SH,
         width=16,
         height=9
 )
 
 ggsave(
-        'output/Fig 01 - Hospitalization rates per 100k/Figure_1_Both.png',
+        'output/Fig 01 - Hospitalization rates per 100k/Figure_1_Both_20220126.png',
         Figure_1_Both,
         width=16,
         height=9
