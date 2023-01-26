1868012e0ecfd677edfd706f8c9aaf8d989788dc
dzanahmed <55654315+dzanahmed@users.noreply.github.com>
2023-01-26 01:40:14 +0000

    Germany preprocessing - merged 3 sources

--- scripts/cleaning/DE/flunet_de.R
+++ scripts/cleaning/DE/flunet_de.R
@@ -20,6 +20,8 @@ VIW_FNT <- read_csv("data/raw_data/FluNet/VIW_FNT.csv")
 #----------------------------------------------------------------
 # Germany
 
+# Setting up helpers
+
 not_all_na <- function(x) any(!is.na(x))
 not_any_na <- function(x) all(!is.na(x))
 
@@ -27,116 +29,163 @@ years <- c(2016:2019, 2022,2023)
 
 germanyPopulation <- read_csv("data/raw_data/Countries_population.csv") 
 
-germanyPopulation <- germanyPopulation |> filter(Code=="DE")
+germanyPopulation <- germanyPopulation |> filter(code=="DE")
 
+source(file="scripts/cleaning/var_order_merged_csv.R")
+
+epiweeks <- read_csv(file = 'data/epi_weeks.csv')
+
+epiweeks <- epiweeks |> rename("Datum" = epi_dates, "week" = epi_wk_no)
+
+# WHO Data: FluID + FluNet ------------------------------------------------
+
+# Hospitalization rate for SARI for all age groups calculated here
 germany_fluID <-
   VIW_FID_EPI |> filter(COUNTRY_CODE == 'DEU' &
-                          MMWR_YEAR %in% years) |> select(where(not_all_na)) #|> 
-  # select(COUNTRY_AREA_TERRITORY,
-  #        MMWR_WEEKSTARTDATE,
-  #        MMWR_YEAR,
-  #        MMWR_WEEK, 
-  #        ORIGIN_SOURCE,
-  #        AGEGROUP_CODE,
-  #        SARI_INPATIENTS,
-  #        SARI_POP_COV)
-  
+                          MMWR_YEAR %in% years) |> select(where(not_all_na)) |>
+  select(
+    COUNTRY_AREA_TERRITORY,
+    MMWR_WEEKSTARTDATE,
+    MMWR_YEAR,
+    MMWR_WEEK,
+    ORIGIN_SOURCE,
+    AGEGROUP_CODE,
+    SARI_INPATIENTS,
+    SARI_POP_COV
+  ) |> drop_na() |>
+  mutate(hsp_rate = SARI_INPATIENTS / SARI_POP_COV * 100000) |>
+  mutate(AGEGROUP_CODE = toupper(AGEGROUP_CODE))
 
-  germany_fluNET <-
-  VIW_FNT |> filter(COUNTRY_CODE == 'DEU' &
-                      MMWR_YEAR %in% years) |> select(where(not_all_na)) # |> select(
-                      #   HEMISPHERE,
-                      #   COUNTRY_CODE,
-                      #   COUNTRY_AREA_TERRITORY,
-                      #   MMWR_WEEKSTARTDATE,
-                      #   MMWR_YEAR,
-                      #   MMWR_WEEK,
-                      #   ORIGIN_SOURCE,
-                      #   SPEC_PROCESSED_NB,
-                      #   RSV,
-                      #   INF_A,
-                      #   INF_B,
-                      #   INF_ALL
-                      # )
+# FluNet has sentinel data. Drop all-NA columns, select what is necessary and filter
+germany_fluNET <-
+  VIW_FNT |> select(where(not_all_na)) |> select(
+                        HEMISPHERE,
+                        COUNTRY_CODE,
+                        COUNTRY_AREA_TERRITORY,
+                        MMWR_WEEKSTARTDATE,
+                        MMWR_YEAR,
+                        MMWR_WEEK,
+                        ORIGIN_SOURCE,
+                        SPEC_PROCESSED_NB,
+                        RSV,
+                        INF_A,
+                        INF_B,
+                        INF_ALL
+                      ) |> filter(COUNTRY_CODE == 'DEU' &
+                                 MMWR_YEAR %in% years & 
+                                   ORIGIN_SOURCE=="SENTINEL")
 
+# FluNet has aggregated age data, no age groups. Set to uppercase ALL to match FluID
+germany_fluNET$AGEGROUP_CODE <- "ALL"
 
-de_data <- merge(germany_fluNET, germany_fluID, by=c("MMWR_WEEKSTARTDATE",
-                                          "MMWR_YEAR",
-                                          "MMWR_WEEK"), all.x=TRUE)
-
-
- germany_covid_age <- read_csv(file="data/raw_data/DE/COVID/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv")
- germany_covid_age <- germany_covid_age |> filter(Bundesland=="Bundesgebiet" & Datum>"2021-12-31")
-
-# This is possibly unnecessary since filter(Altersgruppe=="00+") extracts the same
-germany_covid_total <- read_csv(file = "data/raw_data/DE/COVID/Aktuell_Deutschland_adjustierte-COVID-19-Hospitalisierungen.csv")
-
-germany_covid_total <-  germany_covid_total |> filter(Bundesland == "Bundesgebiet" & Datum > "2021-12-31")
-
-
-# |>
-#   select(
-#     Datum,
-#     Altersgruppe,
-#     aktualisierte_7T_Hospitalisierung_Faelle,
-#     aktualisierte_7T_Hospitalisierung_Inzidenz,
-#     Bevoelkerung
-#   )
-
-
+# Merge these two on week/year/agegroup
+WHO_fluNET_fluID_data <- merge(
+  germany_fluNET,
+  germany_fluID,
+  by = c(
+    "MMWR_WEEKSTARTDATE",
+    "MMWR_YEAR",
+    "MMWR_WEEK",
+    "AGEGROUP_CODE"
+  ),
+  all.x = TRUE,
+  all.y = TRUE
+) |> rename("age_group" = AGEGROUP_CODE,
+            "week" = MMWR_WEEK,
+            "year" = MMWR_YEAR)
 
 
-germany_fid <- VIW_FID_EPI %>% 
-  filter(COUNTRY_CODE=='DEU' & ((MMWR_YEAR >= 2016 & MMWR_YEAR < 2020) | MMWR_YEAR == 2022 | MMWR_YEAR==2023)) %>% 
-  select(HEMISPHERE, COUNTRY_AREA_TERRITORY, MMWR_WEEKSTARTDATE, MMWR_YEAR, MMWR_WEEK, AGEGROUP_CODE, SARI_INPATIENTS)
+# COVID data from RKI GitHub ----------------------------------------------
 
-germany_fid <- germany_fid[order(germany_fid$MMWR_WEEKSTARTDATE),]
-# SARI inpatients is blank for all values -- ignore this dataset
+germany_covid_age <- read_csv(file="data/raw_data/DE/COVID/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv")
 
-germany_fnt <- VIW_FNT %>% 
-  filter(COUNTRY_CODE=='DEU' & ((MMWR_YEAR >= 2016 & MMWR_YEAR < 2020) | MMWR_YEAR == 2022| MMWR_YEAR==2023)) %>% 
-  select(HEMISPHERE, COUNTRY_AREA_TERRITORY, MMWR_WEEKSTARTDATE, MMWR_YEAR, MMWR_WEEK, ORIGIN_SOURCE, SPEC_PROCESSED_NB, RSV, INF_A, INF_B, INF_ALL)
+germany_covid_age <- germany_covid_age |> filter(Bundesland=="Bundesgebiet" & Datum>"2022-01-01") |> arrange(Datum)
 
-germany_fnt <- germany_fnt[order(germany_fnt$MMWR_WEEKSTARTDATE),]
+# Add weeks from epiweeks
+germany_covid_age <- merge(epiweeks, germany_covid_age)
 
-#standardize var names according to data template
-france_fnt$data_source <- "fluNet"
+# Rename german variables
+germany_covid_age <- germany_covid_age |> rename(
+  "hsp_abs_covid" = `7T_Hospitalisierung_Faelle`,
+  "hsp_rate_covid19" = `7T_Hospitalisierung_Inzidenz`,
+  "age_group" = Altersgruppe,
+  "MMWR_WEEKSTARTDATE" = Datum
+) |>
+  select(-Bundesland, -Bundesland_Id) |>
+  mutate(
+    age_group = case_when(
+      age_group == "00+" ~ "ALL",
+      age_group == "00-04" ~ "0TO4",
+      age_group == "05-14" ~ "5TO14",
+      # Other age groups are quite similar....
+      TRUE ~ age_group
+    )
+  )
 
-france_fnt <- france_fnt %>% 
-              rename(hemisphere = HEMISPHERE,
-                     country = COUNTRY_AREA_TERRITORY, 
-                     week = MMWR_WEEK,
-                     year = MMWR_YEAR,
-                     hsp_abs_flu = INF_ALL,
-                     hsp_abs_rsv = RSV,
-                     subtype_a_abs = INF_A,
-                     subtype_b_abs = INF_B,
-                     origin_source = ORIGIN_SOURCE)
 
-# include population to calculate rate per 100,000
-france_fnt$denominator <- ifelse(france_fnt$year==2016, 66688564,
-                                ifelse(france_fnt$year==2017,66883321,
-                                       ifelse(france_fnt$year==2018, 67125071,
-                                              ifelse(france_fnt$year==2019, 67356052,
-                                                     ifelse(france_fnt$year==2022, 67885000, NA))))) 
+# Merge WHO data or Flu and RSV with RKI data on COVID -----------------------------------
 
-#france population data
-# 2016: 66688564
-# 2017: 66883321
-# 2018: 67125071
-# 2019: 67356052 #(https://www.insee.fr/en/statistiques/serie/001641584)
-# 2022: 67885000 #average of the estimated population at the beginning of each month in 2022 (https://www.insee.fr/en/statistiques/serie/001641607)
+WHO_RKI_Hosp_data <-
+ merge(
+   germany_covid_age,
+   WHO_fluNET_fluID_data,
+   by = c("year", "week", "age_group"),
+   all.x = TRUE,
+   all.y = TRUE
+ )
 
-# calculate rate per 100,000
-# RSV rate
-france_fnt$hsp_rate_rsv = (france_fnt$hsp_abs_rsv / france_fnt$denominator)*100000
+# Trim the excess
+WHO_RKI_Hosp_data <- WHO_RKI_Hosp_data |> select(
+ -c(MMWR_WEEKSTARTDATE.x,
+  MMWR_WEEKSTARTDATE.y,
+ COUNTRY_AREA_TERRITORY.x,
+ ORIGIN_SOURCE.x,
+ ORIGIN_SOURCE.y,
+ SPEC_PROCESSED_NB,
+ COUNTRY_AREA_TERRITORY.y,
+ SARI_POP_COV))
+ 
+# Rename
+WHO_RKI_Hosp_data <- WHO_RKI_Hosp_data |> rename(
+ "hemisphere" = HEMISPHERE,
+ "country" = COUNTRY_CODE,
+ "hsp_abs_rsv" = RSV,
+ "subtype_a_abs" = INF_A,
+ "subtype_b_abs" = INF_B,
+ "hsp_abs_flu" = INF_ALL,
+ "hsp_abs" = SARI_INPATIENTS,
+) 
 
-# Flu rate
-france_fnt$hsp_rate_flu = (france_fnt$hsp_abs_flu / france_fnt$denominator)*100000
+# Add denominator / population data
+WHO_RKI_Hosp_data <- WHO_RKI_Hosp_data |> left_join(germanyPopulation[,c("year", "population")], by=c("year"="year"))
+WHO_RKI_Hosp_data <- WHO_RKI_Hosp_data |> rename("denominator"=population)
 
-# reorder vars
-var_order <- c("data_source", "country", "hemisphere", "week", "year", "origin_source", "hsp_rate_flu", "hsp_rate_rsv", "hsp_abs_flu", "hsp_abs_rsv", "denominator", "subtype_a_abs", "subtype_b_abs")
-france_fnt <- france_fnt[,var_order]
+# Add fixed values for DE
+# Calculate Hospitalization rates based on absolute numbers divided by denominator, 
+# multiplied by 100 (1%), and 100,000 for rate
+
+WHO_RKI_Hosp_data <- WHO_RKI_Hosp_data |> mutate(hemisphere = "NH",
+  country = "DE",
+  data_source = "WHO FluNET/FluID+RKI",
+  hsp_rate_flu = 10000000 * (hsp_abs_flu / denominator),
+  hsp_rate_rsv = 10000000 * (hsp_abs_rsv / denominator)
+)
+
+# Set all NAs
+WHO_RKI_Hosp_data$id <- NA
+WHO_RKI_Hosp_data$cases_rate_flu <- NA
+WHO_RKI_Hosp_data$cases_rate_rsv <- NA
+WHO_RKI_Hosp_data$cases_rate_covid19 <- NA
+WHO_RKI_Hosp_data$cases_abs_covid <- NA
+WHO_RKI_Hosp_data$cases_abs_flu <- NA
+WHO_RKI_Hosp_data$cases_abs_rsv <- NA
+WHO_RKI_Hosp_data$subtype_c_abs <- NA
+WHO_RKI_Hosp_data$subtype_a_rate<-NA
+WHO_RKI_Hosp_data$subtype_b_rate<-NA
+WHO_RKI_Hosp_data$subtype_c_rate<-NA
+
+# Reorder variables
+WHO_RKI_Hosp_data[, order_header_premerge]
 
 # save and export
-write_csv(france_fnt, file = "france_fluNet.csv")
+write_csv(WHO_RKI_Hosp_data, file = 'data/processed_data/DE/WHO_RKI_Hosp_data.csv')
