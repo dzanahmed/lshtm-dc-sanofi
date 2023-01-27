8759b0b559475b2e2e55ce0eda43f84014ed9660
dzanahmed <55654315+dzanahmed@users.noreply.github.com>
2023-01-27 01:27:33 +0000

    Germany raw->process->premerge
    Reorganized, wrote README, streamlined everything

--- scripts/cleaning/DE/RKI_Lab_data.R
+++ scripts/cleaning/DE/RKI_Lab_data.R
@@ -1,7 +1,12 @@
-# This is for processing DE Lab data from Robert Koch Institut 
+# This is for processing raw Germany Lab data from Robert Koch Institut 
+
+# Output is a file 'RKI_Lab_data.csv' with year, week, age_group, cases_rate_flu, cases_rate_covid19
+
+library(tidyverse)
+library(stringr)
 
 # All RKI Lab data is here
-dir <- "data/raw_data/DE" 
+dir <- "data/raw_data/DE/RKI_Lab_data" 
 
 files <- list.files(dir, pattern = "*.csv", full.names = TRUE)
 
@@ -26,22 +31,34 @@ for (file in files) {
      all_RKI_data <- rbind(all_RKI_data, df)
 }
 
-all_RKI_data[2:20] <- as.numeric(unlist(all_RKI_data[2:20])) # convert to numeric
-
+# Rename all COV values in pathogen column to covid19 for consistency
 all_RKI_data$pathogen[all_RKI_data$pathogen=="COV"] <- "covid19" # rename
 
-all_RKI_data[is.na(all_RKI_data)] <- 0 # Set all NAs to 0s
+# Set all NAs to 0s
+all_RKI_data[is.na(all_RKI_data)] <- 0 
 
 # Reorder variables and remove Unknown as there are no values present other than 0
 all_RKI_data <- all_RKI_data |> dplyr::select(year, week, pathogen, Total, starts_with('A'))
 
 # Reorient the table to show weeks & age-groups in rows and lab+ incidence by virus in columns
+all_RKI_data <- all_RKI_data |> pivot_longer(names_to = "age_group",
+                                             values_to = "incidence",
+                                             cols = Total:`A80+`) |>
+        pivot_wider(names_from = pathogen, values_from = incidence) |>
+        rename(
+                "cases_rate_flu" = flu,
+                "cases_rate_rsv" = rsv,
+                "cases_rate_covid19" = covid19
+        ) 
+
+# Rename values in age groups
+
 all_RKI_data <-
-        all_RKI_data |> pivot_longer(names_to = "age_group",
-                                     values_to = "incidence",
-                                     cols = Total:`A80+`) |>
-        pivot_wider(names_from = pathogen, values_from = incidence) |> 
-        rename("cases_rate_flu"=flu,"cases_rate_rsv"=rsv,"cases_rate_covid19"=covid19)
+        all_RKI_data |> mutate(age_group = case_when(
+                age_group == "Total" ~ "ALL",
+                age_group != "Total" ~ str_replace_all(age_group, c("\\.." =
+                                                                            "-", "A" = ""))
+        ))
 
 # Write a CSV
-write.csv(all_RKI_data, file = 'data/processed_data/DE/RKI_Lab_data.csv')
+write_csv(all_RKI_data, file = 'data/processed_data/DE/RKI_Lab_data.csv')
