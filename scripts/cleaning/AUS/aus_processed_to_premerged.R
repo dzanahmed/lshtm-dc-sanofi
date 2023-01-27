a33cf122101be31acf62c46ccbc41e4420667de9
jjcato9 <7caterson@gmail.com>
2023-01-27 10:57:00 +0000

    completed aus premerge cleaning script and updates AUS premerged csv

--- scripts/cleaning/AUS/aus_processed_to_premerged.R
+++ scripts/cleaning/AUS/aus_processed_to_premerged.R
@@ -21,7 +21,7 @@ aus_pop <- aus_pop %>% select('year','population')
 aus_pop <- aus_pop %>% filter(year != 2020) %>% filter(year != 2021)
 
 ## merge two datasets
-aus_all_viruses <- merge(aus_flu_rsv,aus_covid, by = c('week','year'),all = T)
+aus_all_viruses <- merge(aus_flu_rsv,aus_covid, by.x = c('week','year'),by.y = c('epi_wk_no','year'),all = T)
 
 ## tidy up some of the columns
 aus_all_viruses$id <- seq(1:nrow(aus_all_viruses))
@@ -33,16 +33,33 @@ aus_all_viruses <- merge(aus_all_viruses, aus_pop, by.x = c('year'),by.y = c('ye
 aus_all_viruses$denominator <- aus_all_viruses$population
 aus_all_viruses$population <- NULL
 
+#match column classes
+template$hsp_abs_covid <- as.character(template$hsp_abs_covid)
+template$denominator <- as.integer(template$denominator)
+
 #combine CSV with template
 aus_premerged <- bind_rows(template,aus_all_viruses)
 
 #tidy up rest of the relevant rows
 aus_premerged$age_group <- 'ALL'
 aus_premerged$denominator <- as.numeric(aus_premerged$denominator)
+aus_premerged$hsp_abs_covid <- as.numeric(gsub(',','',aus_premerged$hsp_abs_covid))
 aus_premerged <- aus_premerged %>% rowwise() %>%
      mutate(hsp_rate_covid19 = hsp_abs_covid/denominator*100000)
 
-## Check if flu/rsv is sentinel or total data
+#calculate hospital rates
+aus_premerged <- aus_premerged %>% rowwise() %>%
+     mutate(hsp_rate_covid19 = hsp_abs_covid/denominator*100000)
+aus_premerged <- aus_premerged %>% rowwise() %>%
+     mutate(hsp_rate_flu = hsp_abs_flu/denominator*100000)
+aus_premerged <- aus_premerged %>% rowwise() %>%
+     mutate(hsp_rate_rsv = hsp_abs_rsv/denominator*100000)
+aus_premerged <- aus_premerged %>% rowwise() %>%
+     mutate(hsp_rate = sum(hsp_rate_covid19,hsp_rate_flu,hsp_rate_rsv,na.rm=T))
+
+#drop unnecessary columns
+aus_premerged$X <- NULL
+aus_premerged$epi_dates <- NULL
 
 #write csv for export
-#write.csv(aus_premerged,'data/premerged_data/AU_premerged_total.csv')
+write.csv(aus_premerged,'data/premerged_data/AU_premerged_total.csv')
