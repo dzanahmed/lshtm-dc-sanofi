271d0f284c6900307daaa39385c561bba7edff36
dzanahmed <55654315+dzanahmed@users.noreply.github.com>
2023-01-27 22:15:19 +0000

    US premerged data script and outputs

--- scripts/cleaning/US/Script_COVID.R
+++ scripts/cleaning/US/Script_COVID.R
@@ -24,13 +24,18 @@ us_covid <- us_covid |> select(
 ) 
 
 # Rename age groups for easier merging with other age stratified files
-us_covid <- us_covid |> mutate(age_group = case_when(age_group==">= 18" ~ "18+", 
-                             age_group=="< 18" ~ "0-17",
-                             age_group=="5-11 " ~ "5-11",
-                             age_group=="0-<6 months" ~ "0-0.5",
-                             age_group=="6 months-4" ~ "0.5-4",
-                             age_group=="Overall" ~ "ALL",
-                             TRUE ~ age_group))
+us_covid <-
+        us_covid |>  mutate(age_group = str_replace_all(age_group, " yr", "")) |>
+        mutate(age_group = case_when(
+                        age_group == ">= 18" ~ "18+",
+                        age_group == "< 18" ~ "0-17",
+                        age_group == "5-11 " ~ "5-11",
+                        age_group == "0-<6 months" ~ "0-0.5",
+                        age_group == "6 months-4" ~ "0.5-4",
+                        age_group == "Overall" ~ "ALL",
+                        TRUE ~ age_group
+                )
+        )
 
 # Drop empty values
 us_covid <- us_covid |> filter(hsp_rate_covid19!="null")
