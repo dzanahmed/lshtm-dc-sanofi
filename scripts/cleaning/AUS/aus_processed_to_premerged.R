604ed18e21ee48cb3f5adf0899f9ffb69bb290ce
jjcato9 <7caterson@gmail.com>
2023-02-08 21:15:43 +0000

    comments updated

--- scripts/cleaning/AUS/aus_processed_to_premerged.R
+++ scripts/cleaning/AUS/aus_processed_to_premerged.R
@@ -1,14 +1,18 @@
+### AUS PROCESSED TO PREMERGED ###
 library(tidyverse)
 library(readr)
 
+#Update working directory as required
 setwd('~/lshtm-dc-sanofi')
 
-#read in csv files for merging
+#read in csv files made in previous step for merging
 aus_covid <- read.csv('data/processed_data/AUS/australia_covid_all_updated.csv')
 aus_flu_rsv <- read.csv('data/processed_data/AUS/australia_fluNet.csv')
 
 ## read in template file for premerged csv
 template <- read.csv('data/template.csv')
+
+#update column types for merging
 template$data_source <- as.character(template$data_source)
 template$country <- as.character(template$country)
 template$hemisphere <- as.character(template$hemisphere)
@@ -23,17 +27,18 @@ aus_pop <- aus_pop %>% filter(year != 2020) %>% filter(year != 2021)
 ## merge two datasets
 aus_all_viruses <- merge(aus_flu_rsv,aus_covid, by.x = c('week','year'),by.y = c('epi_wk_no','year'),all = T)
 
-## tidy up some of the columns
+## add some metadata
 aus_all_viruses$id <- seq(1:nrow(aus_all_viruses))
 aus_all_viruses$data_source <- 'FluNet/AUS DOH'
 aus_all_viruses$country <- 'AUS'
 aus_all_viruses$hemisphere <- 'SH'
 
+#add denominator data
 aus_all_viruses <- merge(aus_all_viruses, aus_pop, by.x = c('year'),by.y = c('year'),all = T)
 aus_all_viruses$denominator <- aus_all_viruses$population
 aus_all_viruses$population <- NULL
 
-#match column classes
+#match column classes before merge
 template$hsp_abs_covid <- as.character(template$hsp_abs_covid)
 template$denominator <- as.integer(template$denominator)
 
@@ -44,8 +49,6 @@ aus_premerged <- bind_rows(template,aus_all_viruses)
 aus_premerged$age_group <- 'ALL'
 aus_premerged$denominator <- as.numeric(aus_premerged$denominator)
 aus_premerged$hsp_abs_covid <- as.numeric(gsub(',','',aus_premerged$hsp_abs_covid))
-aus_premerged <- aus_premerged %>% rowwise() %>%
-     mutate(hsp_rate_covid19 = hsp_abs_covid/denominator*100000)
 
 #calculate hospital rates
 aus_premerged <- aus_premerged %>% rowwise() %>%
