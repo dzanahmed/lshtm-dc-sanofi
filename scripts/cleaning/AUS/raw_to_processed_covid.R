b8ba8b52276ecea868faf43a22b88146280207a4
jjcato9 <7caterson@gmail.com>
2023-02-08 21:12:26 +0000

    updated comments

--- scripts/cleaning/AUS/raw_to_processed_covid.R
+++ scripts/cleaning/AUS/raw_to_processed_covid.R
@@ -1,17 +1,27 @@
+### AUSTRALIA RAW TO PROCESSED COVID ###
 library(tidyverse)
-library(readr)
+
+#change working directory if needed
 setwd('~/lshtm-dc-sanofi')
 
+#read in raw data file
 aus_covid <- read.csv('data/raw_data/AUS/aus_covid_national_update.csv')
+
+#read in epidemiological weeks file
 epi_wks <- read.csv('data/epi_weeks.csv')
+
+#create date column with complete data
 aus_covid <- aus_covid %>% rowwise() %>%
      mutate(date = paste0(Date,'/',X))
 
+#select only data from 2022 onwards
 aus_covid <- aus_covid %>% filter(X >= 2022)
 
+#reformat dates to Date class
 aus_covid$date <- as.Date(aus_covid$date,format='%d/%m/%Y')
 epi_wks$epi_dates <- as.Date(epi_wks$epi_dates)
 
+#merge epi weeks data with australia data by epi_dates/date
 merged_data <- merge(epi_wks, aus_covid, by.x = c('epi_dates'), by.y = c('date'))
 
 #drop unnecessary columns
