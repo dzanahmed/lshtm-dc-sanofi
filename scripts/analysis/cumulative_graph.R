4ed9560f47bff4db3cc594fc8a3c3cae3ed74f75
jjcato9 <7caterson@gmail.com>
2023-02-08 21:37:16 +0000

    Changed csv source, fixed bugs, added comments

--- scripts/analysis/cumulative_graph.R
+++ scripts/analysis/cumulative_graph.R
@@ -1,14 +1,17 @@
+### SUPPLEMENTARY FIGURE - CUMULATIVE GRAPH ###
+### MEDIAN CASE WEEK TABLE ###
+
 #import required libraries
 library(tidyverse)
 library(ggplot2)
 library(cowplot)
 library(forcats)
 
-#set working directory
+#set working directory - amend as required
 setwd('~/lshtm-dc-sanofi')
 
 #read in data
-all_data <- read.csv('data/merged_data/merged_data2.csv')
+all_data <- read.csv('data/merged_data/merged_data.csv')
 
 ### THEMATIC SETTINGS FOR GRAPHS###
 col_seasons_nh <- c('2016/17' = '#8f90b3',
@@ -22,9 +25,11 @@ col_seasons_sh <- c('2016' = '#8f90b3',
                     '2019' = '#dc9334',
                     '2022' = '#DC143C')
 
-#filter for france for analysis
+##### FLU GRAPHS #####
+
+### FRANCE ###
+#filter for France for analysis
 fra_data <- all_data %>% filter(country == 'FR' & data_source == 'FluNet - Sentinel')
-names(fra_data)
 fra_data <- fra_data %>% select('epi_dates','week','year','hsp_abs','hsp_abs_flu','hsp_abs_rsv','hsp_abs_covid')
 fra_data$epi_dates <- as.Date(fra_data$epi_dates)
 
@@ -37,6 +42,7 @@ fra_data <- fra_data %>% mutate(season =
      )
 )
 
+#Drop rows with NA for season as not used for analysis
 fra_szn_data <- fra_data %>% drop_na(season)
 
 #calculate cumulative sums
@@ -44,11 +50,12 @@ fra_szn_data <- fra_szn_data %>%
      group_by(season) %>%
      mutate(cum_flu = cumsum(hsp_abs_flu))
 
-#add seasonal week
+#add seasonal week for x-axis
 fra_szn_data <- fra_szn_data %>% mutate(season_week = 
                                              case_when(week >= 40 ~ week-39,
                                                        week <= 20 ~ week+12))
 
+#Plot graph (with legend to extract for later)
 fra_flu_cum_leg <- ggplot() + geom_line(data=fra_szn_data,
                     aes(x=season_week,y=cum_flu,col=season),size=1.5) + 
      scale_color_manual(values = col_seasons_nh) +
@@ -63,14 +70,16 @@ fra_flu_cum_leg <- ggplot() + geom_line(data=fra_szn_data,
      theme(legend.position = 'right') +
      labs(col = 'Season')
 
-## extract legend then remove
+## extract legend then remove it from the graph
 legend_nh <- get_legend(fra_flu_cum_leg)
 fra_flu_cum <- fra_flu_cum_leg + theme(legend.position = 'none')
 
 ### UK GRAPH ###
+#Extract UK data, selecting relevant columns
 uk_data <- all_data %>% filter(country == 'UK' & age_group == 'ALL')
-
 uk_data <- uk_data %>% select('epi_dates','week','year','hsp_abs','hsp_abs_flu','hsp_abs_rsv','hsp_abs_covid')
+
+#Change to date class
 uk_data$epi_dates <- as.Date(uk_data$epi_dates)
 
 #add column for flu season
@@ -82,6 +91,7 @@ uk_data <- uk_data %>% mutate(season =
                                      )
 )
 
+#Drop season rows with NA values as not for analysis
 uk_szn_data <- uk_data %>% drop_na(season)
 
 #calculate cumulative sums
@@ -94,6 +104,7 @@ uk_szn_data <- uk_szn_data %>% mutate(season_week =
                                              case_when(week >= 40 ~ week-39,
                                                        week <= 20 ~ week+12))
 
+#plot UK cumulatative graph
 uk_flu_cum <- ggplot() + geom_line(data=uk_szn_data,
                      aes(x=season_week,y=cum_flu,col=season),size=1.5) + 
      scale_color_manual(values = col_seasons_nh) +
@@ -108,9 +119,11 @@ uk_flu_cum <- ggplot() + geom_line(data=uk_szn_data,
      theme(legend.position = 'none')
 
 ### AUS ###
-aus_data <- all_data %>% filter(country == 'AUS' & age_group == 'ALL' & data_source == 'fluNet')
-
+#Filter data and relevant columns
+aus_data <- all_data %>% filter(country == 'AUS' & age_group == 'ALL')
 aus_data <- aus_data %>% select('epi_dates','week','year','hsp_abs','hsp_abs_flu','hsp_abs_rsv','hsp_abs_covid')
+
+#Correct Date class
 aus_data$epi_dates <- as.Date(aus_data$epi_dates)
 
 #add column for flu season
@@ -123,6 +136,7 @@ aus_data <- aus_data %>% mutate(season =
                                    )
 )
 
+#Drop season rows with NA as not included in analysis
 aus_szn_data <- aus_data %>% drop_na(season)
 
 #calculate cumulative sums
@@ -130,6 +144,7 @@ aus_szn_data <- aus_szn_data %>%
      group_by(season) %>%
      mutate(cum_flu = cumsum(hsp_abs_flu))
 
+#Plot graph
 aus_flu_cum <- ggplot() + geom_line(data=aus_szn_data,
                      aes(x=week,y=cum_flu,col=season),size=1.5) + 
      scale_color_manual(values = col_seasons_sh) +
@@ -144,9 +159,11 @@ aus_flu_cum <- ggplot() + geom_line(data=aus_szn_data,
      theme(legend.position = 'none')
 
 ### CHILE ###
-chi_data <- all_data %>% filter(country == 'CL' & age_group == 'ALL' & data_source == 'EPI MINSAL - FluNet (Flu cases)')
-
+#Select Chile data and relevant columns
+chi_data <- all_data %>% filter(country == 'CL' & age_group == 'ALL')
 chi_data <- chi_data %>% select('epi_dates','week','year','hsp_abs','hsp_abs_flu','hsp_abs_rsv','hsp_abs_covid')
+
+#Correct Date class
 chi_data$epi_dates <- as.Date(chi_data$epi_dates)
 
 #add column for flu season
@@ -159,6 +176,7 @@ chi_data <- chi_data %>% mutate(season =
                                      )
 )
 
+#Drop season with rows = NA as excluded from analysis
 chi_szn_data <- chi_data %>% drop_na(season)
 
 #calculate cumulative sums
@@ -166,6 +184,7 @@ chi_szn_data <- chi_szn_data %>%
      group_by(season) %>%
      mutate(cum_flu = cumsum(hsp_abs_flu))
 
+#Plot Chile Cumultative Graph with Legend
 chi_flu_cum_leg <- ggplot() + geom_line(data=chi_szn_data,
                      aes(x=week,y=cum_flu,col=season),size=1.5) + 
      scale_color_manual(values = col_seasons_sh) +
@@ -184,12 +203,12 @@ chi_flu_cum_leg <- ggplot() + geom_line(data=chi_szn_data,
 legend_sh <- get_legend(chi_flu_cum_leg)
 chi_flu_cum <- chi_flu_cum_leg + theme(legend.position = 'none')
 
-#Plot Flu Grid
+## Plot Flu Grid ##
 sh_grid <- plot_grid(aus_flu_cum,chi_flu_cum, nrow=1,rel_widths = c(1,0.9))
 nh_grid <- plot_grid(fra_flu_cum,uk_flu_cum,nrow=1,rel_widths = c(1,0.9))
 flu_grid <- plot_grid(sh_grid,nh_grid,nrow=2,rel_heights = c(1,1))
 
-### RSV GRAPHS ###
+##### RSV GRAPHS #####
 
 ## FRANCE ##
 #calculate cumulative sums
@@ -202,6 +221,7 @@ fra_szn_data_rsv <- fra_szn_data_rsv %>% mutate(season_week =
                                              case_when(week >= 40 ~ week-39,
                                                        week <= 20 ~ week+12))
 
+#Plot graph
 fra_rsv_cum <- ggplot() + geom_line(data=fra_szn_data_rsv,
                                     aes(x=season_week,y=cum_rsv,col=season),size=1.5) + 
      scale_color_manual(values = col_seasons_nh) +
@@ -226,6 +246,7 @@ uk_szn_data_rsv <- uk_szn_data_rsv %>% mutate(season_week =
                                            case_when(week >= 40 ~ week-39,
                                                      week <= 20 ~ week+12))
 
+#Plot graph
 uk_rsv_cum <- ggplot() + geom_line(data=uk_szn_data_rsv,
                                    aes(x=season_week,y=cum_rsv,col=season),size=1.5) + 
      scale_color_manual(values = col_seasons_nh) +
@@ -245,6 +266,7 @@ aus_szn_data_rsv <- aus_szn_data %>%
      group_by(season) %>%
      mutate(cum_rsv = cumsum(hsp_abs_rsv))
 
+#Plot graph
 aus_rsv_cum <- ggplot() + geom_line(data=aus_szn_data_rsv,
                                     aes(x=week,y=cum_rsv,col=season),size=1.5) + 
      scale_color_manual(values = col_seasons_sh) +
@@ -264,7 +286,8 @@ chi_szn_data_rsv <- chi_szn_data %>%
      group_by(season) %>%
      mutate(cum_rsv = cumsum(hsp_abs_rsv)) 
 
-chi_rsv_cum_leg <- ggplot() + geom_line(data=chi_szn_data_rsv,
+#Plot graph
+chi_rsv_cum <- ggplot() + geom_line(data=chi_szn_data_rsv,
                                         aes(x=week,y=cum_rsv,col=season),size=1.5) + 
      scale_color_manual(values = col_seasons_sh) +
      theme_bw() +
@@ -281,6 +304,8 @@ chi_rsv_cum_leg <- ggplot() + geom_line(data=chi_szn_data_rsv,
 sh_grid_rsv <- plot_grid(aus_rsv_cum,chi_rsv_cum, nrow=1,rel_widths = c(1,0.9))
 nh_grid_rsv <- plot_grid(fra_rsv_cum,uk_rsv_cum,nrow=1,rel_widths = c(1,0.9))
 rsv_grid <- plot_grid(sh_grid_rsv,nh_grid_rsv,nrow=2,rel_heights = c(1,1))
+
+### Construct final figure using cowplot ###
 
 ### Create Title Frames ###
 title_flu <- ggdraw() + 
@@ -308,6 +333,7 @@ final_grid
 ggsave('output/Fig 02 - Hospitalization rates by season/cumul_figure.png',final_grid,width = 9,height=4.5,bg='white')
 
 ### CALCULATE MEDIAN VALUES BY SEASON - for Tables ###
+
 #create maxima columns by country and virus
 ## Flu
 fra_szn_data <- fra_szn_data %>% group_by(season) %>% mutate(max=max(cum_flu))
@@ -322,99 +348,124 @@ aus_szn_data_rsv <- aus_szn_data_rsv %>% group_by(season) %>% mutate(max=max(cum
 chi_szn_data_rsv <- chi_szn_data_rsv %>% group_by(season) %>% mutate(max=max(cum_rsv))
 
 # AUS FLU
+## Find median week for flu per season
 med_rows <- aus_szn_data %>% 
      filter(cum_flu > (max/2)) %>% 
      group_by(season) %>% 
      summarise(median_case_no = min(cum_flu))
 
+## Get all data for median weeks per season
 median_case_table <- merge(aus_szn_data,med_rows,
                            by.x = c('season','cum_flu'),
                            by.y = c('season','median_case_no'))
 
+#Select relevant columns for table
 median_case_table %>% select(1:4)
 
 #AUS RSV
+## Find median week for rsv per season
 med_rows <- aus_szn_data_rsv %>% 
      filter(cum_rsv > (max/2)) %>% 
      group_by(season) %>% 
      summarise(median_case_no = min(cum_rsv))
 
+## Get all data for median weeks per season
 median_case_table <- merge(aus_szn_data_rsv,med_rows,
                            by.x = c('season','cum_rsv'),
                            by.y = c('season','median_case_no'))
 
+#Select relevant columns for table
 median_case_table %>% select(1:4)
 
 # CHI FLU
+## Find median week for flu per season
 med_rows <- chi_szn_data %>% 
      filter(cum_flu > (max/2)) %>% 
      group_by(season) %>% 
      summarise(median_case_no = min(cum_flu))
 
+## Get all data for median weeks per season
 median_case_table <- merge(chi_szn_data,med_rows,
                            by.x = c('season','cum_flu'),
                            by.y = c('season','median_case_no'))
 
+#Select relevant columns for table
 median_case_table %>% select(1:4)
 
 #CHI RSV
+## Find median week for rsv per season
 med_rows <- chi_szn_data_rsv %>% 
      filter(cum_rsv > (max/2)) %>% 
      group_by(season) %>% 
      summarise(median_case_no = min(cum_rsv))
 
+## Get all data for median weeks per season
 median_case_table <- merge(chi_szn_data_rsv,med_rows,
                            by.x = c('season','cum_rsv'),
                            by.y = c('season','median_case_no'))
 
+#Select relevant columns for table
 median_case_table %>% select(1:4)
 
+## NORTHERN HEMISPHERE ##
 # FRA FLU
+## Find median week for flu per season
 med_rows <- fra_szn_data %>% 
      filter(cum_flu > (max/2)) %>% 
      group_by(season) %>% 
      summarise(median_case_no = min(cum_flu))
 
+## Get all data for median weeks per season
 median_case_table <- merge(fra_szn_data,med_rows,
                            by.x = c('season','cum_flu'),
                            by.y = c('season','median_case_no'))
 
+#Select relevant columns for table
 median_case_table %>% select(1:4)
 
 # FRA RSV
+## Find median week for rsv per season
 med_rows <- fra_szn_data_rsv %>% 
      filter(cum_rsv > (max/2)) %>% 
      group_by(season) %>% 
      summarise(median_case_no = min(cum_rsv))
 
+## Get all data for median weeks per season
 median_case_table <- merge(fra_szn_data_rsv,med_rows,
                            by.x = c('season','cum_rsv'),
                            by.y = c('season','median_case_no'))
 
+#Select relevant columns for table
 median_case_table %>% select(1:4)
 
 # UK FLU
+## Find median week for flu per season
 med_rows <- uk_szn_data %>% 
      filter(cum_flu > (max/2)) %>% 
      group_by(season) %>% 
      summarise(median_case_no = min(cum_flu))
 
+## Get all data for median weeks per season
 median_case_table <- merge(uk_szn_data,med_rows,
                            by.x = c('season','cum_flu'),
                            by.y = c('season','median_case_no'))
 
+#Select relevant columns for table
 median_case_table %>% select(1:4)
 
 # UK RSV
+## Find median week for rsv per season
 med_rows <- uk_szn_data_rsv %>% 
      filter(cum_rsv > (max/2)) %>% 
      group_by(season) %>% 
      summarise(median_case_no = min(cum_rsv))
 
+## Get all data for median weeks per season
 median_case_table <- merge(uk_szn_data_rsv,med_rows,
                            by.x = c('season','cum_rsv'),
                            by.y = c('season','median_case_no'))
 
+#Select relevant columns for table
 median_case_table %>% select(1:4)
 
                                                          
